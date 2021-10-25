56e3e3bdc540c7396e76dadc96faa9a80c800fad
Paulo Silveira <paulossnjf@gmail.com>
2021-10-25 18:19:40 -0300

    Navbar finalizada (sem o responsivo)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Layout from "../components/layout/Layout";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 };
 
 export default MyApp;
